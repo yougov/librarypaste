8bc0dd9cb43d7608b56cdde20423011df63922b1
Jason R. Coombs <jaraco@jaraco.com>
2012-03-09 14:11:11 -0500

    Removed excess indentation

--- setup.py
+++ setup.py
@@ -3,28 +3,28 @@ from setuptools import setup
 import librarypaste
 
 setup(
-        name='librarypaste',
-        version=librarypaste.version,
-        author='Jamie Turner',
-        author_email='jamie@jamwt.com',
-        url='http://bitbucket.org/chmullig/librarypaste/',
-        description='Simple pastebin',
-        long_description='Simple pastebin',
-        license='MIT',
-        packages=['librarypaste'],
-        package_dir={'librarypaste': 'librarypaste'},
-        include_package_data=True,
-        entry_points={
-            'console_scripts': [
-                'librarypaste=librarypaste.librarypaste:main',
-            ],
-        },
-        install_requires=[
-            'pygments',
-            'Mako',
-            'simplejson',
-            'cherrypy',
-            'routes < 1.12',
+    name='librarypaste',
+    version=librarypaste.version,
+    author='Jamie Turner',
+    author_email='jamie@jamwt.com',
+    url='http://bitbucket.org/chmullig/librarypaste/',
+    description='Simple pastebin',
+    long_description='Simple pastebin',
+    license='MIT',
+    packages=['librarypaste'],
+    package_dir={'librarypaste': 'librarypaste'},
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'librarypaste=librarypaste.librarypaste:main',
         ],
-        zip_safe=False,
+    },
+    install_requires=[
+        'pygments',
+        'Mako',
+        'simplejson',
+        'cherrypy',
+        'routes < 1.12',
+    ],
+    zip_safe=False,
 )
