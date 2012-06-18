39bf08b1e1acd8551798cc590c091b1e3700fcb7
Jason R. Coombs <jaraco@jaraco.com>
2012-06-17 22:16:50 -0400

    Reindented another long line

--- librarypaste/datastore.py
+++ librarypaste/datastore.py
@@ -16,7 +16,8 @@ from random import choice
 def shortkey():
     firstlast = list(letters + digits)
     middle = firstlast + list('-_')
-    return ''.join((choice(firstlast), choice(middle), choice(middle), choice(middle), choice(firstlast)))
+    return ''.join((choice(firstlast), choice(middle), choice(middle),
+        choice(middle), choice(firstlast)))
 
 def init_datastore(config):
     """
