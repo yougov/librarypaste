c7c52a1e9954d4904681e85c9a584d318e51aa2c
Jason R. Coombs <jaraco@jaraco.com>
2012-06-18 09:29:48 -0400

    Added tests for mongodb (failing due to unimplemented method

---
+++ librarypaste/tests/test_mongostore.py
@@ -0,0 +1,22 @@
+import pytest
+import pymongo
+
+from . import DataStoreTest
+from librarypaste import mongostore
+
+class TestMongoDBDataStore(DataStoreTest):
+	datastore = None
+
+	@classmethod
+	def setup_class(cls):
+		try:
+			cls.datastore = mongostore.MongoDBDataStore(
+				'mongodb://localhost/librarypaste-test')
+		except pymongo.errors.AutoReconnect as e:
+			pytest.skip("Local MongoDB not available ({msg})".format(
+				msg = e))
+
+	@classmethod
+	def teardown_class(cls):
+		if cls.datastore:
+			cls.datastore.drop_database()
