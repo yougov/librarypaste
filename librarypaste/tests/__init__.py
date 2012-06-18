162d9b33c1a70cee8832d261a856c32fd3e1ac85
Jason R. Coombs <jaraco@jaraco.com>
2012-06-18 09:15:40 -0400

    Added sample content for tests

--- librarypaste/tests/__init__.py
+++ librarypaste/tests/__init__.py
@@ -1,6 +1,28 @@
 import abc
+import datetime
+
+import pkg_resources
 
 class DataStoreTest(object):
+	common_content = dict(
+		nick = 'nick',
+		time = datetime.datetime.now(),
+		makeshort = True,
+	)
+	code_content = common_content.copy()
+	file_content = common_content.copy()
+	code_content.update(
+		type='code',
+		fmt = 'python',
+		code = pkg_resources.resource_string('librarypaste', 'pastebin.py'),
+	)
+	file_content.update(
+		type = 'file',
+		mime = 'image/png',
+		filename = 'librarypaste.png',
+		data = pkg_resources.resource_string('librarypaste',
+			'static/librarypaste.png')
+	)
 	@abc.abstractproperty
 	def datastore(self):
 		"""
