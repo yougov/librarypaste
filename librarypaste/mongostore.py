99f9b08416360034fc9aaf0b4ec62aba7277040d
Jason R. Coombs <jaraco@jaraco.com>
2012-06-18 23:19:11 -0400

    Allow database name to be specified

--- librarypaste/mongostore.py
+++ librarypaste/mongostore.py
@@ -4,32 +4,38 @@ import bson
 from datastore import DataStore
 
 class MongoDBDataStore(pymongo.Connection, DataStore):
+    db_name = 'librarypaste'
+
+    @property
+    def db(self):
+        return self[self.db_name]
+
     def _store(self, uid, content, data=None):
         """Store the given dict of content at uid. Nothing returned."""
         doc = dict(uid=uid)
         if data:
             doc.update(data=bson.Binary(data))
         doc.update(content)
-        self.librarypaste.pastes.save(doc)
+        self.db.pastes.save(doc)
 
     def _storeLog(self, nick, time, uid):
         """Adds the nick & uid to the log for a given time/order. No return."""
         query = dict(uid=uid)
         update = {'$set': dict(nick=nick, time=time)}
-        self.librarypaste.pastes.update(query, update)
+        self.db.pastes.update(query, update)
 
     def _retrieve(self, uid):
         """Return a dict with the contents of the paste, including the raw
         data, if any, as the key 'data'. Must pass in uid, not shortid."""
         query = dict(uid=uid)
-        return self.librarypaste.pastes.find_one(query)
+        return self.db.pastes.find_one(query)
 
     def lookup(self, nick):
         """Looks for the most recent paste by a given nick.
         Returns the uid or None"""
         query = dict(nick=nick)
         order = dict(time=pymongo.DESCENDING)
-        recs = self.librarypaste.pastes.find(query).order(order).limit(1)
+        recs = self.db.pastes.find(query).order(order).limit(1)
         try:
             return next(recs)['uid']
         except StopIteration:
@@ -37,5 +43,5 @@ class MongoDBDataStore(pymongo.Connection, DataStore):
 
     def _lookupUid(self, shortid):
         query = dict(shortid=shortid)
-        rec = self.librarypaste.pastes.find_one(query)
+        rec = self.db.pastes.find_one(query)
         return rec['uid']
