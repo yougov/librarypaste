cc051c2fa8447831226a62894bb979af3e666235
Jason R. Coombs <jaraco@jaraco.com>
2015-02-07 15:53:22 -0500

    Normalize whitespace

--- librarypaste/tests/__init__.py
+++ librarypaste/tests/__init__.py
@@ -5,25 +5,25 @@ import pkg_resources
 
 class DataStoreTest(object):
 	common_content = dict(
-		nick = 'nick',
-		time = datetime.datetime.now().replace(
+		nick='nick',
+		time=datetime.datetime.now().replace(
 			# some tests fail at the microsecond precision (jsonstore)
 			microsecond=0,
 		),
-		makeshort = True,
+		makeshort=True,
 	)
 	code_content = common_content.copy()
 	file_content = common_content.copy()
 	code_content.update(
 		type='code',
-		fmt = 'python',
-		code = pkg_resources.resource_string('librarypaste', 'pastebin.py'),
+		fmt='python',
+		code=pkg_resources.resource_string('librarypaste', 'pastebin.py'),
 	)
 	file_content.update(
-		type = 'file',
-		mime = 'image/png',
-		filename = 'librarypaste.png',
-		data = pkg_resources.resource_string('librarypaste',
+		type='file',
+		mime='image/png',
+		filename='librarypaste.png',
+		data=pkg_resources.resource_string('librarypaste',
 			'static/librarypaste.png')
 	)
 	@abc.abstractproperty
