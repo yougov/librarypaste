76cd8171f9f18e39fefc781300e7f504ee050bed
Jason R. Coombs <jason.coombs@yougov.com>
2018-04-05 15:08:42 -0400

    Suppress test failures

--- uptests/web/responding.py
+++ uptests/web/responding.py
@@ -5,11 +5,16 @@ import argparse
 
 import portend
 
-parser = argparse.ArgumentParser()
-parser.add_argument('host')
-parser.add_argument('port', type=int)
-args = parser.parse_args()
 
-portend.occupied(args.host, args.port, timeout=3)
-root = 'http://{host}:{port}/'.format(**vars(args))
-urllib.request.urlopen(root)
+def run():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('host')
+	parser.add_argument('port', type=int)
+	args = parser.parse_args()
+
+	portend.occupied(args.host, args.port, timeout=3)
+	root = 'http://{host}:{port}/'.format(**vars(args))
+	urllib.request.urlopen(root)
+
+
+__name__ == '__main__' and run()
